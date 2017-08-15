88909745c24985d522c867b2ea24136e6676f8f5
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-08-14 19:36:08 -0700

    Simple app with basic calls using AJAX.

---
+++ AjaxDemo/Migrations/20170815022515_Initial.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Migrations
+{
+    [DbContext(typeof(AjaxDemoContext))]
+    [Migration("20170815022515_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AjaxDemo.Models.Destination", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("City");
+
+                    b.Property<string>("Country");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Destinations");
+                });
+        }
+    }
+}
---
+++ AjaxDemo/Migrations/AjaxDemoContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Migrations
+{
+    [DbContext(typeof(AjaxDemoContext))]
+    partial class AjaxDemoContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AjaxDemo.Models.Destination", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("City");
+
+                    b.Property<string>("Country");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Destinations");
+                });
+        }
+    }
+}
